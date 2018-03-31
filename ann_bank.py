f3ea89bfafd1d27fb6b4f608f0c59df4b8f4a82c
Ajinkya Ambatwar <37268357+ajinkyaambatwar@users.noreply.github.com>
2018-03-31 11:21:01 +0530

    New changes\\
    Tried to cover Bias variance tradeoff and also looked at Overfiiting of the model. Parameter tuning is now covered in this.

--- ann_bank.py
+++ ann_bank.py
@@ -24,7 +24,8 @@ x[:,1]=le_x_1.fit_transform(x[: ,1]) #label encoder object linked with the 2nd c
 le_x_2 = le()             #label encoder object created for gender
 x[:,2]=le_x_2.fit_transform(x[: ,2])
 ohec=ohe(categorical_features=[1]) #index of the column is to be specified for the onehot encoding
-x = ohec.fit_transform(x).toarray() #now we have to fit the ohec object into
+x=ohec.fit_transform(x).toarray()
+#now we have to fit the ohec object into
 x=x[:,1:]                           #to eliminate the dummy variable trap(like for three classes a dummy variable set of 2 is fine(third is automatically set))
 
 #data splitting
@@ -40,6 +41,7 @@ x_test = sc_x.transform(x_test)
 import keras as ke 
 from keras.models import Sequential #to initialize the ann
 from keras.layers import Dense      #to build the layers of the ann
+from keras.layers import Dropout as dr
 
 #INITIALIZING THE ANN
 classifier=Sequential()             #sequential object created( ann as sequence of layers)
@@ -48,11 +50,15 @@ classifier=Sequential()             #sequential object created( ann as sequence 
 classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="uniform"))           #now we will give the no of nodes in hidden layer, activation function=rectifier function,initialized value for the weights=uniform distribution
 '''we generally prefer the rectfier function as the activation function in case of the hidden layer activation and sigmoid function for the 
 output layer activation'''
+'''Dropout probability should vary from 0.1 till max of 0.5 and if we go too beyond that
+we won't have a neural netowrk at all'''
+
+classifier.add(dr(rate=0.1))
 #now we need to add the sencond hidden layer
 classifier.add(Dense(activation="relu",units=6, kernel_initializer="uniform"))           #now for the second hidden layer no need of input dimensions as first hidden layer is already created so the output of that layer is the input to this layer(so input_dim=11)
 '''now the dimension of the output layer for 2nd hidden layer is the 2nd hidden layer itself.
 so we will better use the dimension to be average of the input and the output layer dimensions'''
-
+classifier.add(dr(rate=0.1))
 #now we will add the the output layer
 classifier.add(Dense(activation="sigmoid", units=1,kernel_initializer="uniform"))       #activ_func=suftmax(or somethong like that) if we have output in multiple categories
 '''output has the sigmoid function activation'''
@@ -99,3 +105,63 @@ print(pred)
 '''the explained method in the solution-
 new_prediction=classifier.predict(np.array([[info of the new customer]]))'''
 
+#Evaluating,improving snd tuning the ANN
+'''We have tested the acuracy of our model using a fixed test set and
+this doesn't give us a clear insight of its accuracy considering 
+bias and variance problems'''
+
+#Evaluating the ANN
+from keras.wrappers.scikit_learn import KerasClassifier    #Implementation of the scikit-learn classifier API for Keras
+from sklearn.model_selection import cross_val_score        #Evaluate a cross validation score
+
+'''KerasClassifier taken a function as parameter'''
+def fun_build():
+    classifier=Sequential()             #sequential object created( ann as sequence of layers)
+    classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="uniform"))           #now we will give the no of nodes in hidden layer, activation function=rectifier function,initialized value for the weights=uniform distribution
+    classifier.add(Dense(activation="relu",units=6, kernel_initializer="uniform"))           #now for the second hidden layer no need of input dimensions as first hidden layer is already created so the output of that layer is the input to this layer(so input_dim=11)
+    classifier.add(Dense(activation="sigmoid", units=1,kernel_initializer="uniform"))       #activ_func=suftmax(or somethong like that) if we have output in multiple categories
+    classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+    return classifier 
+
+classifer=KerasClassifier(build_fn=fun_build,batch_size=10,epochs=100)      #kerasclassifer object to implement sklearn
+accuracies=cross_val_score(classifer,x_train,y_train,cv=10,n_jobs=-1)                 #no. of folds=10
+'''n_jobs=number of cpus to be used(-1=All CPUS used)
+After this we will get 10 different accuracies as we have run k-fold validation 10 times.
+10 different cross validation sets. IN each iteration, one part of the training set is 
+test set and rest 10 are training sets. So we will be getting 10 different accuracies relating
+to 10 different chosen pairs. Now the final accuracy is taken to be the mean of them.
+'''
+mean=accuracies.mean()
+variance=accuracies.std()
+'''Variance is to be less.And mean accuracy has to be increased now'''
+
+
+'''NOw we can handle the problem of overfitting i.e high variance in accuracies vector'''
+#Improving ANN
+#Regularization dropout (if you remember the lambda parameter)
+'''For fropout reference refer to https://www.quora.com/How-does-the-dropout-method-work-in-deep-learning-And-why-is-it-claimed-to-be-an-effective-trick-to-improve-your-network'''
+'''Retention probability for input layer is around 1 and for hidden layer is around 0.5.Implemenation up'''
+
+#parameter tuning
+'''we will be tuning the parameters including batch size and no. of epochs to get the maximum accuracy'''
+from keras.wrappers.scikit_learn import KerasClassifier    #Implementation of the scikit-learn classifier API for Keras
+from sklearn.model_selection import GridSearchCV as gc        #Evaluate a cross validation score
+
+'''KerasClassifier taken a function as parameter'''
+def fun_build(optim):
+    classifier=Sequential()             #sequential object created( ann as sequence of layers)
+    classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="uniform"))           #now we will give the no of nodes in hidden layer, activation function=rectifier function,initialized value for the weights=uniform distribution
+    classifier.add(Dense(activation="relu",units=6, kernel_initializer="uniform"))           #now for the second hidden layer no need of input dimensions as first hidden layer is already created so the output of that layer is the input to this layer(so input_dim=11)
+    classifier.add(Dense(activation="sigmoid", units=1,kernel_initializer="uniform"))       #activ_func=suftmax(or somethong like that) if we have output in multiple categories
+    classifier.compile(optimizer=optim,loss='binary_crossentropy',metrics=['accuracy'])
+    return classifier 
+
+classifer=KerasClassifier(build_fn=fun_build) 
+parameters={'batch_size':[25,32],'epochs':[100,500],'optim':['adam','rmsprop']}
+
+grid_search=gc(estimator=classifer,param_grid=parameters,scoring='accuracy',cv=10)
+grid_search=grid_search.fit(x_train,y_train)
+best_parameters=grid_search.best_params_
+best_accuracy=grid_search.best_score_
+
+#Grid search does the work
